830a7cad3f3bedc9ea5c6ac388e60b3147c7d241
jmacey <jmacey@bournemouth.ac.uk>
2022-11-07 09:20:15 +0000

    Updated for NGL9 master branch updated to main. Need to get some new meshes for the demos as these are really old and broken

--- MeshToNGL/src/NGLScene.cpp
+++ MeshToNGL/src/NGLScene.cpp
@@ -14,9 +14,6 @@
 #include <assimp/postprocess.h>
 #include <assimp/vector3.h>
 
-
-
-
 NGLScene::NGLScene(const std::string &_fname)
 {
   setTitle("Using libassimp with NGL simple Mesh");
@@ -25,33 +22,28 @@ NGLScene::NGLScene(const std::string &_fname)
   // we are taking one of the postprocessing presets to avoid
   // spelling out 20+ single postprocessing flags here.
 
-	m_scene = aiImportFile(_fname.c_str(),
-												 aiProcessPreset_TargetRealtime_MaxQuality |
-												 aiProcess_Triangulate |
-												 aiProcess_PreTransformVertices |
-												 aiProcess_FixInfacingNormals
-												 );
-  if(m_scene == nullptr)
-	{
-			std::cout<<"error opening file "<<_fname<<"\n";
-			exit(EXIT_FAILURE);
-	}
-
+  m_scene = aiImportFile(_fname.c_str(),
+                         aiProcessPreset_TargetRealtime_MaxQuality |
+                             aiProcess_Triangulate |
+                             aiProcess_PreTransformVertices |
+                             aiProcess_FixInfacingNormals);
+  if (m_scene == nullptr)
+  {
+    std::cout << "error opening file " << _fname << "\n";
+    exit(EXIT_FAILURE);
+  }
 }
-
 
 NGLScene::~NGLScene()
 {
-  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+  std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
-
-
-void NGLScene::resizeGL(int _w , int _h)
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_project=ngl::perspective(45.0f,static_cast<float>(_w)/_h,0.5f,550.0f);
-  m_win.width=static_cast<int>(_w*devicePixelRatio());
-  m_win.height=static_cast<int>(_h*devicePixelRatio());
+  m_project = ngl::perspective(45.0f, static_cast<float>(_w) / _h, 0.5f, 550.0f);
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 void NGLScene::initializeGL()
 {
@@ -59,7 +51,7 @@ void NGLScene::initializeGL()
   // gl commands from the lib, if this is not done program will crash
   ngl::NGLInit::initialize();
 
-  glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
   // enable multisampling for smoother drawing
@@ -67,69 +59,66 @@ void NGLScene::initializeGL()
   // Now we will create a basic Camera from the graphics library
   // This is a static camera so it only needs to be set once
   // First create Values for the camera position
-  ngl::Vec3 min,max;
-  AIU::getSceneBoundingBox(m_scene,min,max);
-  ngl::Vec3 center=(min+max)/2.0f;
+  ngl::Vec3 min, max;
+  AIU::getSceneBoundingBox(m_scene, min, max);
+  ngl::Vec3 center = (min + max) / 2.0f;
   ngl::Vec3 from;
-  from.m_x=0.0f;
-  from.m_y=max.m_y*4.0f;
-  from.m_z=max.m_z*4.0f;
-  std::cout<<"from "<<from<<" center "<<center<<"\n";
+  from.m_x = 0.0f;
+  from.m_y = max.m_y * 4.0f;
+  from.m_z = max.m_z * 4.0f;
+  std::cout << "from " << from << " center " << center << "\n";
 
   // now load to our new camera
-  m_view=ngl::lookAt(from,center,ngl::Vec3::up());
+  m_view = ngl::lookAt(from, center, ngl::Vec3::up());
 
   // now to load the shader and set the values
- // we are creating a shader called Phong to save typos
- // in the code create some constexpr
- constexpr auto shaderProgram="Phong";
- constexpr auto vertexShader="PhongVertex";
- constexpr auto fragShader="PhongFragment";
- // create the shader program
- ngl::ShaderLib::createShaderProgram(shaderProgram);
- // now we are going to create empty shaders for Frag and Vert
- ngl::ShaderLib::attachShader(vertexShader,ngl::ShaderType::VERTEX);
- ngl::ShaderLib::attachShader(fragShader,ngl::ShaderType::FRAGMENT);
- // attach the source
- ngl::ShaderLib::loadShaderSource(vertexShader,"shaders/PhongVertex.glsl");
- ngl::ShaderLib::loadShaderSource(fragShader,"shaders/PhongFragment.glsl");
- // compile the shaders
- ngl::ShaderLib::compileShader(vertexShader);
- ngl::ShaderLib::compileShader(fragShader);
- // add them to the program
- ngl::ShaderLib::attachShaderToProgram(shaderProgram,vertexShader);
- ngl::ShaderLib::attachShaderToProgram(shaderProgram,fragShader);
+  // we are creating a shader called Phong to save typos
+  // in the code create some constexpr
+  constexpr auto shaderProgram = "Phong";
+  constexpr auto vertexShader = "PhongVertex";
+  constexpr auto fragShader = "PhongFragment";
+  // create the shader program
+  ngl::ShaderLib::createShaderProgram(shaderProgram);
+  // now we are going to create empty shaders for Frag and Vert
+  ngl::ShaderLib::attachShader(vertexShader, ngl::ShaderType::VERTEX);
+  ngl::ShaderLib::attachShader(fragShader, ngl::ShaderType::FRAGMENT);
+  // attach the source
+  ngl::ShaderLib::loadShaderSource(vertexShader, "shaders/PhongVertex.glsl");
+  ngl::ShaderLib::loadShaderSource(fragShader, "shaders/PhongFragment.glsl");
+  // compile the shaders
+  ngl::ShaderLib::compileShader(vertexShader);
+  ngl::ShaderLib::compileShader(fragShader);
+  // add them to the program
+  ngl::ShaderLib::attachShaderToProgram(shaderProgram, vertexShader);
+  ngl::ShaderLib::attachShaderToProgram(shaderProgram, fragShader);
 
- // now we have associated that data we can link the shader
- ngl::ShaderLib::linkProgramObject(shaderProgram);
- // and make it active ready to load values
- ngl::ShaderLib::use(shaderProgram);
- ngl::Vec4 lightPos=from;
- ngl::Mat4 iv=m_view;
- iv.inverse().transpose();
- ngl::ShaderLib::setUniform("light.position",lightPos*iv);
- ngl::ShaderLib::setUniform("light.ambient",0.1f,0.1f,0.1f,1.0f);
- ngl::ShaderLib::setUniform("light.diffuse",1.0f,1.0f,1.0f,1.0f);
- ngl::ShaderLib::setUniform("light.specular",0.8f,0.8f,0.8f,1.0f);
- // gold like phong material
- ngl::ShaderLib::setUniform("material.ambient",0.274725f,0.1995f,0.0745f,0.0f);
- ngl::ShaderLib::setUniform("material.diffuse",0.75164f,0.60648f,0.22648f,0.0f);
- ngl::ShaderLib::setUniform("material.specular",0.628281f,0.555802f,0.3666065f,0.0f);
- ngl::ShaderLib::setUniform("material.shininess",51.2f);
- ngl::ShaderLib::setUniform("viewerPos",from);
-
+  // now we have associated that data we can link the shader
+  ngl::ShaderLib::linkProgramObject(shaderProgram);
+  // and make it active ready to load values
+  ngl::ShaderLib::use(shaderProgram);
+  ngl::Vec4 lightPos = from;
+  ngl::Mat4 iv = m_view;
+  iv.inverse().transpose();
+  ngl::ShaderLib::setUniform("light.position", lightPos * iv);
+  ngl::ShaderLib::setUniform("light.ambient", 0.1f, 0.1f, 0.1f, 1.0f);
+  ngl::ShaderLib::setUniform("light.diffuse", 1.0f, 1.0f, 1.0f, 1.0f);
+  ngl::ShaderLib::setUniform("light.specular", 0.8f, 0.8f, 0.8f, 1.0f);
+  // gold like phong material
+  ngl::ShaderLib::setUniform("material.ambient", 0.274725f, 0.1995f, 0.0745f, 0.0f);
+  ngl::ShaderLib::setUniform("material.diffuse", 0.75164f, 0.60648f, 0.22648f, 0.0f);
+  ngl::ShaderLib::setUniform("material.specular", 0.628281f, 0.555802f, 0.3666065f, 0.0f);
+  ngl::ShaderLib::setUniform("material.shininess", 51.2f);
+  ngl::ShaderLib::setUniform("viewerPos", from);
 
   buildVAOFromScene();
   // as re-size is not explicitly called we need to do this.
-  m_project=ngl::perspective(45.0f,static_cast<float>(width())/height(),0.5f,550.0f);
-
+  m_project = ngl::perspective(45.0f, static_cast<float>(width()) / height(), 0.5f, 550.0f);
 }
 
 void NGLScene::buildVAOFromScene()
 {
 
-recurseScene(m_scene,m_scene->mRootNode,ngl::Mat4(1.0));
-
+  recurseScene(m_scene, m_scene->mRootNode, ngl::Mat4(1.0));
 }
 
 // a simple structure to hold our vertex data
@@ -145,67 +134,68 @@ struct vertData
   GLfloat v;
 };
 
-void NGLScene::recurseScene(const aiScene *sc, const aiNode *nd,const ngl::Mat4 &_parentTx)
+void NGLScene::recurseScene(const aiScene *sc, const aiNode *nd, const ngl::Mat4 &_parentTx)
 {
-  ngl::Mat4 m=AIU::aiMatrix4x4ToNGLMat4Transpose(nd->mTransformation);
+  ngl::Mat4 m = AIU::aiMatrix4x4ToNGLMat4Transpose(nd->mTransformation);
 
   unsigned int n = 0, t;
-  std::vector <vertData> verts;
+  std::vector<vertData> verts;
   vertData v;
   meshItem thisMesh;
   // the transform is relative to the parent node so we accumulate
-  thisMesh.tx=m*_parentTx;
+  thisMesh.tx = m * _parentTx;
 
   for (; n < nd->mNumMeshes; ++n)
   {
     verts.clear();
-    const aiMesh* mesh = m_scene->mMeshes[nd->mMeshes[n]];
+    const aiMesh *mesh = m_scene->mMeshes[nd->mMeshes[n]];
 
-    thisMesh.vao=ngl::VAOFactory::createVAO(ngl::simpleVAO,GL_TRIANGLES);
+    thisMesh.vao = ngl::VAOFactory::createVAO(ngl::simpleVAO, GL_TRIANGLES);
     for (t = 0; t < mesh->mNumFaces; ++t)
     {
-      const aiFace* face = &mesh->mFaces[t];
+      const aiFace *face = &mesh->mFaces[t];
       // only deal with triangles for ease
-      if(face->mNumIndices !=3){std::cout<<"mesh size not tri"<<face->mNumIndices<<"\n"; break;}
-      for(unsigned int i = 0; i < face->mNumIndices; i++)
+      if (face->mNumIndices != 3)
+      {
+        std::cout << "mesh size not tri" << face->mNumIndices << "\n";
+        break;
+      }
+      for (unsigned int i = 0; i < face->mNumIndices; i++)
       {
         unsigned int index = face->mIndices[i];
 
-
-        if(mesh->mNormals != nullptr)
+        if (mesh->mNormals != nullptr)
         {
-          v.nx= mesh->mNormals[index].x;
-          v.ny= mesh->mNormals[index].y;
-          v.nz= mesh->mNormals[index].z;
+          v.nx = mesh->mNormals[index].x;
+          v.ny = mesh->mNormals[index].y;
+          v.nz = mesh->mNormals[index].z;
         }
-        if(mesh->HasTextureCoords(0))
+        if (mesh->HasTextureCoords(0))
         {
-          v.u=mesh->mTextureCoords[0]->x;
-          v.v=mesh->mTextureCoords[0]->y;
-
+          v.u = mesh->mTextureCoords[0]->x;
+          v.v = mesh->mTextureCoords[0]->y;
         }
-        v.x=mesh->mVertices[index].x;
-        v.y=mesh->mVertices[index].y;
-        v.z=mesh->mVertices[index].z;
+        v.x = mesh->mVertices[index].x;
+        v.y = mesh->mVertices[index].y;
+        v.z = mesh->mVertices[index].z;
         verts.push_back(v);
       }
-
     }
     thisMesh.vao->bind();
     // now we have our data add it to the VAO, we need to tell the VAO the following
     // how much (in bytes) data we are copying
     // a pointer to the first element of data (in this case the address of the first element of the
     // std::vector
-    thisMesh.vao->setData(ngl::AbstractVAO::VertexData(verts.size()*sizeof(vertData),verts[0].x));
+    thisMesh.vao->setData(ngl::AbstractVAO::VertexData(verts.size() * sizeof(vertData), verts[0].x));
     // in this case we have packed our data in interleaved format as follows
     // u,v,nx,ny,nz,x,y,z
     // If you look at the shader we have the following attributes being used
     // attribute vec3 inVert; attribute 0
     // attribute vec3 inNormal; attribure 1
     // attribute vec2 inUV; attribute 2
-    thisMesh.vao->setVertexAttributePointer(0,3,GL_FLOAT,sizeof(vertData),0);
-    thisMesh.vao->setVertexAttributePointer(1,3,GL_FLOAT,sizeof(vertData),3);
-    thisMesh.vao->setVertexAttributePointer(2,2,GL_FLOAT,sizeof(vertData),6);
+    thisMesh.vao->setVertexAttributePointer(0, 3, GL_FLOAT, sizeof(vertData), 0);
+    thisMesh.vao->setVertexAttributePointer(1, 3, GL_FLOAT, sizeof(vertData), 3);
+    thisMesh.vao->setVertexAttributePointer(2, 2, GL_FLOAT, sizeof(vertData), 6);
     thisMesh.vao->setNumIndices(verts.size());
     // finally we have finished for now so time to unbind the VAO
     thisMesh.vao->unbind();
@@ -215,51 +205,47 @@ void NGLScene::recurseScene(const aiScene *sc, const aiNode *nd,const ngl::Mat4 
   // draw all children
   for (n = 0; n < nd->mNumChildren; ++n)
   {
-    recurseScene(sc, nd->mChildren[n],thisMesh.tx);
+    recurseScene(sc, nd->mChildren[n], thisMesh.tx);
   }
-
 }
-
 
 void NGLScene::loadMatricesToShader()
 {
-  
+
   ngl::Mat4 MV;
   ngl::Mat4 MVP;
   ngl::Mat3 normalMatrix;
   ngl::Mat4 M;
-  M   = m_mouseGlobalTX * m_transform.getMatrix();
-  MV  = m_view * M;
-  MVP = m_project* MV;
-  normalMatrix=MV;
+  M = m_mouseGlobalTX * m_transform.getMatrix();
+  MV = m_view * M;
+  MVP = m_project * MV;
+  normalMatrix = MV;
   normalMatrix.inverse().transpose();
-  ngl::ShaderLib::setUniform("MV",MV);
-  ngl::ShaderLib::setUniform("MVP",MVP);
-  ngl::ShaderLib::setUniform("normalMatrix",normalMatrix);
-  ngl::ShaderLib::setUniform("M",M);
+  ngl::ShaderLib::setUniform("MV", MV);
+  ngl::ShaderLib::setUniform("MVP", MVP);
+  ngl::ShaderLib::setUniform("normalMatrix", normalMatrix);
+  ngl::ShaderLib::setUniform("M", M);
 }
 
 void NGLScene::paintGL()
 {
   // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glViewport(0,0,m_win.width,m_win.height);
+  glViewport(0, 0, m_win.width, m_win.height);
   ngl::ShaderLib::use("Phong");
 
   // Rotation based on the mouse position for our global transform
   ngl::Transformation trans;
-  ngl::Mat4 rotX;
-  ngl::Mat4 rotY;
-  // create the rotation matrices
-  rotY.rotateY(m_win.spinYFace);
+  auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
+  auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
   // multiply the rotations
-  m_mouseGlobalTX=rotY*rotX;
+  m_mouseGlobalTX = rotY * rotX;
   // add the translations
   m_mouseGlobalTX.m_m[3][0] = m_modelPos.m_x;
   m_mouseGlobalTX.m_m[3][1] = m_modelPos.m_y;
   m_mouseGlobalTX.m_m[3][2] = m_modelPos.m_z;
   // set this in the TX stack
-  for(auto &m :m_meshes)
+  for (auto &m : m_meshes)
   {
     ngl::Transformation t;
     t.setMatrix(m.tx);
@@ -270,7 +256,6 @@ void NGLScene::paintGL()
   }
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------
 
 void NGLScene::keyPressEvent(QKeyEvent *_event)
@@ -280,18 +265,29 @@ void NGLScene::keyPressEvent(QKeyEvent *_event)
   switch (_event->key())
   {
   // escape key to quite
-  case Qt::Key_Escape : QGuiApplication::exit(EXIT_SUCCESS); break;
+  case Qt::Key_Escape:
+    QGuiApplication::exit(EXIT_SUCCESS);
+    break;
   // turn on wirframe rendering
-  case Qt::Key_W : glPolygonMode(GL_FRONT_AND_BACK,GL_LINE); break;
+  case Qt::Key_W:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    break;
   // turn off wire frame
-  case Qt::Key_S : glPolygonMode(GL_FRONT_AND_BACK,GL_FILL); break;
+  case Qt::Key_S:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    break;
   // show full screen
-  case Qt::Key_F : showFullScreen(); break;
+  case Qt::Key_F:
+    showFullScreen();
+    break;
   // show windowed
-  case Qt::Key_N : showNormal(); break;
-  default : break;
+  case Qt::Key_N:
+    showNormal();
+    break;
+  default:
+    break;
   }
   // finally update the GLWindow and re-draw
-  //if (isExposed())
-    update();
+  // if (isExposed())
+  update();
 }
--- SkeletalAnimation/src/NGLScene.cpp
+++ SkeletalAnimation/src/NGLScene.cpp
@@ -18,24 +18,21 @@
 NGLScene::NGLScene(const char *_fname)
 {
   setTitle("Using libassimp with NGL for Animation");
-  m_animate=true;
-  m_frameTime=0.0f;
-  m_sceneName=_fname;
+  m_animate = true;
+  m_frameTime = 0.0f;
+  m_sceneName = _fname;
 }
-
 
 NGLScene::~NGLScene()
 {
-  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+  std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
-
-
-void NGLScene::resizeGL(int _w , int _h)
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_project=ngl::perspective(45.0f,static_cast<float>(_w)/_h,0.05f,350.0f);
-  m_win.width=static_cast<int>(_w*devicePixelRatio());
-  m_win.height=static_cast<int>(_h*devicePixelRatio());
+  m_project = ngl::perspective(45.0f, static_cast<float>(_w) / _h, 0.05f, 350.0f);
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 void NGLScene::initializeGL()
 {
@@ -46,126 +43,117 @@ void NGLScene::initializeGL()
   ngl::VAOFactory::registerVAOCreator("multiBufferIndexVAO", MultiBufferIndexVAO::create);
   ngl::VAOFactory::listCreators();
 
-  glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
   // enable multisampling for smoother drawing
   glEnable(GL_MULTISAMPLE);
   m_scene = m_importer.ReadFile(m_sceneName,
                                 aiProcessPreset_TargetRealtime_Quality |
-                                aiProcess_Triangulate
-                                );
-  if(m_scene == nullptr)
+                                    aiProcess_Triangulate);
+  if (m_scene == nullptr)
   {
-    std::cerr<<"Error loading scene file\n";
+    std::cerr << "Error loading scene file\n";
     exit(EXIT_FAILURE);
   }
-  std::cout<<"num animations "<<m_scene->mNumAnimations<<"\n";
-  m_numAnimations=m_scene->mNumAnimations;
-  if(m_scene->mNumAnimations <1)
+  std::cout << "num animations " << m_scene->mNumAnimations << "\n";
+  m_numAnimations = m_scene->mNumAnimations;
+  if (m_scene->mNumAnimations < 1)
   {
-      std::cerr<<"No animations in this scene exiting\n";
-      exit(EXIT_FAILURE);
+    std::cerr << "No animations in this scene exiting\n";
+    exit(EXIT_FAILURE);
   }
-  bool loaded=m_mesh.load(m_scene);
-  if(loaded == false)
+  bool loaded = m_mesh.load(m_scene);
+  if (loaded == false)
   {
-      std::cerr<<"Assimp reports "<<m_importer.GetErrorString()<<"\n";
-      exit(EXIT_FAILURE);
+    std::cerr << "Assimp reports " << m_importer.GetErrorString() << "\n";
+    exit(EXIT_FAILURE);
   }
   // now to load the shader and set the values
   // we are creating a shader called Skinning use string to avoid typos
-  auto constexpr Skinning="Skinning";
-  auto constexpr SkinningVertex="SkinningVertex";
-  auto constexpr SkinningFragment="SkinningFragment";
+  auto constexpr Skinning = "Skinning";
+  auto constexpr SkinningVertex = "SkinningVertex";
+  auto constexpr SkinningFragment = "SkinningFragment";
 
   ngl::ShaderLib::createShaderProgram(Skinning);
   // now we are going to create empty shaders for Frag and Vert
-  ngl::ShaderLib::attachShader(SkinningVertex,ngl::ShaderType::VERTEX);
-  ngl::ShaderLib::attachShader(SkinningFragment,ngl::ShaderType::FRAGMENT);
+  ngl::ShaderLib::attachShader(SkinningVertex, ngl::ShaderType::VERTEX);
+  ngl::ShaderLib::attachShader(SkinningFragment, ngl::ShaderType::FRAGMENT);
   // attach the source
-  ngl::ShaderLib::loadShaderSource(SkinningVertex,"shaders/SkinningVertex.glsl");
-  ngl::ShaderLib::loadShaderSource(SkinningFragment,"shaders/SkinningFragment.glsl");
+  ngl::ShaderLib::loadShaderSource(SkinningVertex, "shaders/SkinningVertex.glsl");
+  ngl::ShaderLib::loadShaderSource(SkinningFragment, "shaders/SkinningFragment.glsl");
   // compile the shaders
   ngl::ShaderLib::compileShader(SkinningVertex);
   ngl::ShaderLib::compileShader(SkinningFragment);
   // add them to the program
-  ngl::ShaderLib::attachShaderToProgram(Skinning,SkinningVertex);
-  ngl::ShaderLib::attachShaderToProgram(Skinning,SkinningFragment);
+  ngl::ShaderLib::attachShaderToProgram(Skinning, SkinningVertex);
+  ngl::ShaderLib::attachShaderToProgram(Skinning, SkinningFragment);
   // now bind the shader attributes for most NGL primitives we use the following
   // now we have associated this data we can link the shader
   ngl::ShaderLib::linkProgramObject(Skinning);
   ngl::ShaderLib::printRegisteredUniforms(Skinning);
   ngl::ShaderLib::use(Skinning);
 
-  ngl::Vec3 min,max;
-  AIU::getSceneBoundingBox(m_scene,min,max);
-  ngl::Vec3 center=(min+max)/2.0f;
+  ngl::Vec3 min, max;
+  AIU::getSceneBoundingBox(m_scene, min, max);
+  ngl::Vec3 center = (min + max) / 2.0f;
   ngl::Vec3 from;
-  from.m_x=0;
-  from.m_y=max.m_y*4.0f;
-  from.m_z=max.m_z*4.0f;
-  std::cout<<"from "<<from<<" center "<<center<<"\n";
+  from.m_x = 0;
+  from.m_y = max.m_y * 4.0f;
+  from.m_z = max.m_z * 4.0f;
+  std::cout << "from " << from << " center " << center << "\n";
   // now load to our new camera
-  m_view=ngl::lookAt(from,center,ngl::Vec3::up());
+  m_view = ngl::lookAt(from, center, ngl::Vec3::up());
   // set the shape using FOV 45 Aspect Ratio based on Width and Height
   // The final two are near and far clipping planes of 0.5 and 10
-  m_project=ngl::perspective(45.0f,720.0f/576.0f,0.05f,350.0f);
+  m_project = ngl::perspective(45.0f, 720.0f / 576.0f, 0.05f, 350.0f);
 
-  ngl::Vec4 lightPos=from;
-  ngl::Mat4 iv=m_view;
+  ngl::Vec4 lightPos = from;
+  ngl::Mat4 iv = m_view;
   iv.inverse().transpose();
-  ngl::ShaderLib::setUniform("light.position",lightPos*iv);
-  ngl::ShaderLib::setUniform("light.ambient",0.1f,0.1f,0.1f,1.0f);
-  ngl::ShaderLib::setUniform("light.diffuse",1.0f,1.0f,1.0f,1.0f);
-  ngl::ShaderLib::setUniform("light.specular",0.8f,0.8f,0.8f,1.0f);
+  ngl::ShaderLib::setUniform("light.position", lightPos * iv);
+  ngl::ShaderLib::setUniform("light.ambient", 0.1f, 0.1f, 0.1f, 1.0f);
+  ngl::ShaderLib::setUniform("light.diffuse", 1.0f, 1.0f, 1.0f, 1.0f);
+  ngl::ShaderLib::setUniform("light.specular", 0.8f, 0.8f, 0.8f, 1.0f);
   // gold like phong material
-  ngl::ShaderLib::setUniform("material.ambient",0.274725f,0.1995f,0.0745f,0.0f);
-  ngl::ShaderLib::setUniform("material.diffuse",0.75164f,0.60648f,0.22648f,0.0f);
-  ngl::ShaderLib::setUniform("material.specular",0.628281f,0.555802f,0.3666065f,0.0f);
-  ngl::ShaderLib::setUniform("material.shininess",51.2f);
-  ngl::ShaderLib::setUniform("viewerPos",from);
-
-
+  ngl::ShaderLib::setUniform("material.ambient", 0.274725f, 0.1995f, 0.0745f, 0.0f);
+  ngl::ShaderLib::setUniform("material.diffuse", 0.75164f, 0.60648f, 0.22648f, 0.0f);
+  ngl::ShaderLib::setUniform("material.specular", 0.628281f, 0.555802f, 0.3666065f, 0.0f);
+  ngl::ShaderLib::setUniform("material.shininess", 51.2f);
+  ngl::ShaderLib::setUniform("viewerPos", from);
 
   // now create our light this is done after the camera so we can pass the
   // transpose of the projection matrix to the light to do correct eye space
   // transformations
- startTimer(20);
+  startTimer(20);
 }
-
-
-
 
 void NGLScene::loadMatricesToShader()
 {
-    ngl::Mat4 MV;
-    ngl::Mat4 MVP;
-    ngl::Mat4 M;
-    M   = m_mouseGlobalTX * m_transform.getMatrix();
-    MV  = m_view * M;
-    MVP = m_project * MV;
-    ngl::ShaderLib::setUniform("MV",MV);
-    ngl::ShaderLib::setUniform("MVP",MVP);
-    ngl::ShaderLib::setUniform("M",M);
+  ngl::Mat4 MV;
+  ngl::Mat4 MVP;
+  ngl::Mat4 M;
+  M = m_mouseGlobalTX * m_transform.getMatrix();
+  MV = m_view * M;
+  MVP = m_project * MV;
+  ngl::ShaderLib::setUniform("MV", MV);
+  ngl::ShaderLib::setUniform("MVP", MVP);
+  ngl::ShaderLib::setUniform("M", M);
 }
 
 void NGLScene::paintGL()
 {
-  glViewport(0,0,m_win.width,m_win.height);
+  glViewport(0, 0, m_win.width, m_win.height);
   // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   ngl::ShaderLib::use("Skinning");
 
   // Rotation based on the mouse position for our global transform
   ngl::Transformation trans;
-  ngl::Mat4 rotX;
-  ngl::Mat4 rotY;
-  // create the rotation matrices
-  rotX.rotateX(m_win.spinXFace);
-  rotY.rotateY(m_win.spinYFace);
+  auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
+  auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
   // multiply the rotations
-  m_mouseGlobalTX=rotY*rotX;
+  m_mouseGlobalTX = rotY * rotX;
   // add the translations
   m_mouseGlobalTX.m_m[3][0] = m_modelPos.m_x;
   m_mouseGlobalTX.m_m[3][1] = m_modelPos.m_y;
@@ -173,10 +161,10 @@ void NGLScene::paintGL()
   // set this in the TX stack
   loadMatricesToShader();
   std::vector<ngl::Mat4> transforms;
-  if(m_animate)
+  if (m_animate)
   {
-    QTime t=QTime::currentTime();
-    float time=float(t.msec())/1000.0f*float(m_mesh.getDuration()/m_mesh.getTicksPerSec());
+    QTime t = QTime::currentTime();
+    float time = float(t.msec()) / 1000.0f * float(m_mesh.getDuration() / m_mesh.getTicksPerSec());
     m_mesh.boneTransform(time, transforms);
   }
   else
@@ -184,18 +172,15 @@ void NGLScene::paintGL()
     m_mesh.boneTransform(m_frameTime, transforms);
   }
 
-  auto size=transforms.size();
-  for (unsigned int i = 0 ; i < size ; ++i)
+  auto size = transforms.size();
+  for (unsigned int i = 0; i < size; ++i)
   {
-    std::string name=fmt::format("gBones[{0}]", i );
-    ngl::ShaderLib::setUniform(name,transforms[i]);
+    std::string name = fmt::format("gBones[{0}]", i);
+    ngl::ShaderLib::setUniform(name, transforms[i]);
   }
-
-
 
   m_mesh.render();
 }
-
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -206,30 +191,41 @@ void NGLScene::keyPressEvent(QKeyEvent *_event)
   switch (_event->key())
   {
   // escape key to quite
-  case Qt::Key_Escape : QGuiApplication::exit(EXIT_SUCCESS); break;
+  case Qt::Key_Escape:
+    QGuiApplication::exit(EXIT_SUCCESS);
+    break;
   // turn on wirframe rendering
-  case Qt::Key_W : glPolygonMode(GL_FRONT_AND_BACK,GL_LINE); break;
+  case Qt::Key_W:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    break;
   // turn off wire frame
-  case Qt::Key_S : glPolygonMode(GL_FRONT_AND_BACK,GL_FILL); break;
+  case Qt::Key_S:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    break;
   // show full screen
-  case Qt::Key_F : showFullScreen(); break;
+  case Qt::Key_F:
+    showFullScreen();
+    break;
   // show windowed
-  case Qt::Key_N : showNormal(); break;
-  case Qt::Key_Left :
+  case Qt::Key_N:
+    showNormal();
+    break;
+  case Qt::Key_Left:
     --m_activeAnimation;
-    m_activeAnimation=std::clamp(m_activeAnimation,size_t(0), m_numAnimations-1);
+    m_activeAnimation = std::clamp(m_activeAnimation, size_t(0), m_numAnimations - 1);
     m_mesh.setActiveAnimation(m_activeAnimation);
 
-  break;
-  case Qt::Key_Right :
+    break;
+  case Qt::Key_Right:
     ++m_activeAnimation;
-    m_activeAnimation=std::clamp(m_activeAnimation,size_t(0), m_numAnimations-1);
+    m_activeAnimation = std::clamp(m_activeAnimation, size_t(0), m_numAnimations - 1);
     m_mesh.setActiveAnimation(m_activeAnimation);
-  break;
+    break;
 
-  default : break;
+  default:
+    break;
   }
-    update();
+  update();
 }
 
 void NGLScene::timerEvent(QTimerEvent *)
